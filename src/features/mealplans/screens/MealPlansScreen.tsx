dd6f09a6cf1c7a033bdeb1f043b4edbde6f230dc
OliverKeefe <oliverkeefe@tutanota.com>
2025-04-16 18:04:35 +0100

    feat(features/mealplans): Add MealPlansScreen

---
+++ src/features/mealplans/screens/MealPlansScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from "react-native";
+
+const MealPlansScreen: React.FC = () => {
+    return(
+        <View>
+            <Text>
+                this is the Meal Plan Screen.
+            </Text>
+        </View>
+    );
+}
+
+export default MealPlansScreen;
