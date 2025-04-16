2e677d4261f2ffa0d92e02ac1506e5621e4ab3c0
OliverKeefe <oliverkeefe@tutanota.com>
2025-04-16 18:06:08 +0100

    feat(features/data): Add DataScreen

---
+++ src/features/data/screens/DataScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const DataScreen: React.FC = () => {
+    return (
+        <View>
+            <Text>
+                This is the Data Screen.
+            </Text>
+        </View>
+    );
+}
