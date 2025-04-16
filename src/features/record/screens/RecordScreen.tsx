b55cb63fc90de7683526d312e7e7337d0aa80ede
OliverKeefe <oliverkeefe@tutanota.com>
2025-04-16 17:33:43 +0100

    feat(features/record): Add RecordScreen

---
+++ src/features/record/screens/RecordScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from "react-native";
+
+const RecordScreen: React.FC = () => {
+    return (
+        <View>
+            <Text>
+                This is the record Screen.
+            </Text>
+        </View>
+    );
+}
+
+export default RecordScreen;
