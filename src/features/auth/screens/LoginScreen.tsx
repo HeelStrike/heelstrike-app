a2af5bf039d99371e7e3d1c33a68a365d2f1fd22
OliverKeefe <oliverkeefe@tutanota.com>
2025-04-16 18:08:16 +0100

    feat(features/auth): Add LoginScreen

---
+++ src/features/auth/screens/LoginScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const LoginScreen: React.FC = () => {
+    return (
+        <View>
+            <Text>
+                This is the Login Screen.
+            </Text>
+        </View>
+    );
+}
+
+export default LoginScreen;
