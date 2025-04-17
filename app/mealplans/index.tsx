75f076dec227f0c57c3e8c2c99486ad7913a3a91
OliverKeefe <oliverkeefe@tutanota.com>
2025-04-17 13:48:16 +0100

    feat(app/mealplans): Add index

---
+++ app/mealplans/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import MealPlansScreen from "@/src/features/mealplans/screens/MealPlansScreen";
+
+export default function MealPlans() {
+    return <MealPlansScreen />
+}
