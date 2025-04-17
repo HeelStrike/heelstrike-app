56d0eb53f23f3cfc874dc5e88f79080fdcfe107a
OliverKeefe <oliverkeefe@tutanota.com>
2025-04-17 20:45:29 +0100

    feat(app): Update _layout, add DataScreen icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,49 @@
-import { Stack } from "expo-router";
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function RootLayout() {
-  return <Stack />;
+export default function Layout() {
+  return (
+      <Tabs>
+          <Tabs.Screen
+            name="index"
+            options={{
+              title: 'Home',
+              tabBarIcon: ({ color, size }) => (
+                  <Ionicons name="home-outline" size={size} color={color} />
+              ),
+            }}
+        />
+
+          <Tabs.Screen
+              name="workouts/index"
+              options={{
+                  title: 'Workouts',
+                  tabBarIcon: ({ color, size }) => (
+                      <Ionicons name="barbell-outline" size={size} color={color} />
+                  ),
+              }}
+          />
+
+          <Tabs.Screen
+              name="record/index"
+              options={{
+                  title: 'Record',
+                  tabBarIcon: ({ color, size }) => (
+                      <Ionicons name="stopwatch-outline" size={size} color={color} />
+                  ),
+              }}
+          />
+
+          <Tabs.Screen
+              name="data/index"
+              options={{
+                  title: 'Data',
+                  tabBarIcon: ({ color, size }) => (
+                      <Ionicons name="analytics-outline" size={size} color={color} />
+                  ),
+              }}
+          />
+
+      </Tabs>
+  );
 }
