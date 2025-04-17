f0df37e3e31a2e03285f0a81630e27cce53cdc3f
OliverKeefe <oliverkeefe@tutanota.com>
2025-04-17 20:44:31 +0100

    feat(navigation): Add DataScreen Tab

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import WorkoutScreen from '@/src/features/workouts/screens/WorkoutScreen';
 import RecordScreen from "@/src/features/record/screens/RecordScreen";
+import DataScreen from "@/src/features/data/screens/DataScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,7 +12,8 @@ const AppNavigator = () => {
     return (
         <Stack.Navigator initialRouteName="Workouts">
             <Stack.Screen name="Workouts" component={WorkoutScreen} />
-            <Stack.Screen name={"Record"} component={RecordScreen} />
+            <Stack.Screen name="Record" component={RecordScreen} />
+            <Stack.Screen name="Data" component={DataScreen} />
         </Stack.Navigator>
     );
 }
