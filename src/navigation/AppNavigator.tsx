fcbcecf6354fc4a507fd9af535f1465b21ad8143
OliverKeefe <oliverkeefe@tutanota.com>
2025-04-19 15:48:32 +0100

    feat(navigation): Add Maps Stack to AppNavigator

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -5,15 +5,17 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import WorkoutScreen from '@/src/features/workouts/screens/WorkoutScreen';
 import RecordScreen from "@/src/features/record/screens/RecordScreen";
 import DataScreen from "@/src/features/data/screens/DataScreen";
+import MapsScreen from "@/src/features/maps/screens/MapsScreen";
 
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
     return (
-        <Stack.Navigator initialRouteName="Workouts">
+        <Stack.Navigator initialRouteName="Maps">
             <Stack.Screen name="Workouts" component={WorkoutScreen} />
             <Stack.Screen name="Record" component={RecordScreen} />
             <Stack.Screen name="Data" component={DataScreen} />
+            <Stack.Screen name="Maps" component={MapsScreen} />
         </Stack.Navigator>
     );
 }
