437b325b60323e16110d8c5f45e403b565a83e7d
OliverKeefe <oliverkeefe@tutanota.com>
2025-04-25 19:08:21 +0100

    feat(app): Add nutrition route to AppNavigator

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -6,16 +6,18 @@ import WorkoutScreen from '@/src/features/workouts/screens/WorkoutScreen';
 import RecordScreen from "@/src/features/record/screens/RecordScreen";
 import DataScreen from "@/src/features/data/screens/DataScreen";
 import MapsScreen from "@/src/features/maps/screens/MapsScreen";
+import NutritionScreen from "@/src/features/nutrition/screens/NutritionScreen";
 
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
     return (
-        <Stack.Navigator initialRouteName="Maps">
+        <Stack.Navigator initialRouteName="Data">
             <Stack.Screen name="Workouts" component={WorkoutScreen} />
             <Stack.Screen name="Record" component={RecordScreen} />
             <Stack.Screen name="Data" component={DataScreen} />
             <Stack.Screen name="Maps" component={MapsScreen} />
+            <Stack.Screen name="Maps" component={NutritionScreen} />
         </Stack.Navigator>
     );
 }
