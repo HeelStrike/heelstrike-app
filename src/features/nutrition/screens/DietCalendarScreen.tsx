1dcc5effd168178ecc51397f520aa5d929167076
OliverKeefe <oliverkeefe@tutanota.com>
2025-04-25 19:10:50 +0100

    feat(features/nutrition): Add DietCalendarScreen

---
+++ src/features/nutrition/screens/DietCalendarScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from "react-native";
+
+const DietCalendarScreen: React.FC = () => {
+    return(
+        <View>
+            <Text>
+                this is the Diet Calendar Screen.
+            </Text>
+        </View>
+    );
+}
+
+export default DietCalendarScreen;
