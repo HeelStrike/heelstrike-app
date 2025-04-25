501f25b02350c120eadf01afa7f23972ea32af1b
OliverKeefe <oliverkeefe@tutanota.com>
2025-04-25 19:08:58 +0100

    chore(app): Remove MealPlans index file

--- app/mealplans/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import MealPlansScreen from "@/src/features/mealplans/screens/MealPlansScreen";
-
-export default function MealPlans() {
-    return <MealPlansScreen />
-}
