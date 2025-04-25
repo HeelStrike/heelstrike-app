03c6d0fb4769203e53cc59dccff776d4c4e4ce0f
OliverKeefe <oliverkeefe@tutanota.com>
2025-04-25 19:11:19 +0100

    feat(features/nutrition): Add ShoppingListScreen

---
+++ src/features/nutrition/screens/ShoppingListScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from "react-native";
+
+const ShoppingListScreen: React.FC = () => {
+    return(
+        <View>
+            <Text>
+                this is the Shopping List Screen.
+            </Text>
+        </View>
+    );
+}
+
+export default ShoppingListScreen;
