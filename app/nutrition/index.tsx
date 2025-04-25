53da73cc5e35fb87f28d54bb3d15f8793fb3e035
OliverKeefe <oliverkeefe@tutanota.com>
2025-04-25 19:09:18 +0100

    feat(app): Add Nutrition index file

---
+++ app/nutrition/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import MealPlansScreen from "@/src/features/nutrition/screens/MealPlansScreen";
+import NutritionScreen from "@/src/features/nutrition/screens/NutritionScreen";
+
+export default function Nutrition() {
+    return <NutritionScreen />
+}
